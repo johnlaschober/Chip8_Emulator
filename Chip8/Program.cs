e9acaf01965fab6fd805eba3bd0021600a373edd
johnlaschober <johnlaschober@gmail.com>
2019-04-16 09:33:51 -0500

    form and pixel drawing

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Chip8;
 
+using System.Windows.Forms;
+using System.Drawing;
+
 namespace Chip8
 {
     class Program
@@ -12,11 +15,21 @@ namespace Chip8
         static void Main(string[] args)
         {
             Chip8 chippy = new Chip8();
+            Form chipForm = new Form();
+            chipForm.Width = 640;
+            chipForm.Height = 320;
+            chipForm.Show();
+            Brush whiteBrush = Brushes.White;
+            Graphics g = chipForm.CreateGraphics();
+            g.Clear(Color.Black);
+
+            
+
             chippy.Initailize();
             chippy.EmulateCycle();
 
             if (chippy.drawFlag) {
-                DrawGraphics(chippy.gfx);
+                DrawGraphics(chippy.gfx, g, whiteBrush);
             }
 
             chippy.SetKeys();
@@ -25,8 +38,18 @@ namespace Chip8
             // right now we are emulating 1 frame only
         }
 
-        static void DrawGraphics(byte[,] gfx) {
+        static void DrawGraphics(byte[] gfx, Graphics g, Brush b) {
             // draw graphics here using a form
+            // 64 x 32
+
+            int x = 0, y = 0;
+            for (int i = 0; i < gfx.Length; i++)
+            {
+                x = i % 64;
+                y = i / 64;
+                g.FillRectangle(b, x*10, y*10, 1*10, 1*10);
+            }
+
         }
     }
 }
